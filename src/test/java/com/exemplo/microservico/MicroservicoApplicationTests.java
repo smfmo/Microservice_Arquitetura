5b143ded41a1eac3ef568b020d2ddc0426b9960e
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-04-09 10:39:25 -0300

    implementação do Docker, subindo a aplicação e o database para o docker

--- src/test/java/com/exemplo/microservico/MicroservicoApplicationTests.java
+++ src/test/java/com/exemplo/microservico/MicroservicoApplicationTests.java
@@ -3,11 +3,14 @@ package com.exemplo.microservico;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class MicroservicoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
